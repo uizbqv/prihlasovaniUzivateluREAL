b290fec7eab527af2edd51a69348d5019bade24c
Honziks <c23slesingr@oakostelec.cz>
2025-11-30 11:09:02 +0100

    AdminVýpis

--- PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/Admin_Vypsat_Users.Designer.cs
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/Admin_Vypsat_Users.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Admin_Vypsat_Users";
+            button1 = new Button();
+            UsersListBox = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            button1.Location = new Point(329, 388);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Show users";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // UsersListBox
+            // 
+            UsersListBox.FormattingEnabled = true;
+            UsersListBox.ItemHeight = 15;
+            UsersListBox.Location = new Point(103, 111);
+            UsersListBox.Name = "UsersListBox";
+            UsersListBox.Size = new Size(607, 244);
+            UsersListBox.TabIndex = 2;
+            // 
+            // Admin_Vypsat_Users
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(UsersListBox);
+            Controls.Add(button1);
+            Name = "Admin_Vypsat_Users";
+            Text = "Admin_Vypsat_Users";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private ListBox UsersListBox;
     }
 }
--- PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/Admin_Vypsat_Users.cs
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/Admin_Vypsat_Users.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,23 @@ namespace PrihlasovaniUzivatelu
         public Admin_Vypsat_Users()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string json = File.ReadAllText("users.json");
+
+            //Půjčím si Users z registeredUser (Od martina)
+            var users = JsonSerializer.Deserialize<List<RegisteredUser>>(json);
+
+            //Vyčistím listbox
+            UsersListBox.Items.Clear();
+
+            //Přidám username do listboxu
+            foreach (var user in users)
+            {
+                UsersListBox.Items.Add(user.username);
+            }
         }
     }
 }
